430d8ada8d95c16d79d782c9fe01763e416831f5
George Vigelette <gvigelet@duvitech.com>
2026-01-05 23:35:03 -0500

    update with extensions needed for vscode

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -5,18 +5,15 @@ set(CMAKE_C_COMPILER_ID GNU)
 set(CMAKE_CXX_COMPILER_ID GNU)
 
 # Some default GCC settings
-# arm-none-eabi- must be part of path environment dt
-set(TOOLCHAIN_ROOT /opt/toolchains/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin)
+# arm-none-eabi- must be part of path environment
 set(TOOLCHAIN_PREFIX                arm-none-eabi-)
 
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}gcc)
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_LINKER                    ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_AR                        ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}ar)
-set(CMAKE_RANLIB                    ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}ranlib)
-set(CMAKE_OBJCOPY                   ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_SIZE                      ${TOOLCHAIN_ROOT}/${TOOLCHAIN_PREFIX}size)
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++)
+set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
+set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
 
 set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
